6e033dd98a1e9d75ded6df704af5bc563f99f4ff
cotne <ge21480014@eu.edu.ge>
2021-09-26 09:35:07 -0700

    sawyisebi

---
+++ kvira 1/nawili_1.py
@@ -0,0 +1,67 @@
+#davaleba_1
+print("Hello World")
+#davaleba_2
+print("ცოტნე აბუსერიძე")
+#davaleba_3
+print("ნიგოითის სკოლა,ნიგოითი,ემზარ ჩიჩუა")
+#davaleba_4
+print("Hello world, Hello world, Hello world, Hello world, Hello world,")
+print("Hello world,Hello world,Hello world,Hello world,")
+print("Hello world,Hello world,Hello world,")
+print("Hello world,")
+print("Hello world,Hello world,")
+print("Hello world,Hello world,Hello world,")
+print("Hello world,Hello world,Hello world,Hello world,")
+print("Hello world,Hello world,Hello world,Hello world,Hello world,")
+#davaleba_5
+print("C++")
+print("C#")
+print("java")
+print("pascal")
+print("php")
+print("JavaScript")
+print("ActionScript")
+print("python")
+#davaleba_6
+x = 5
+y = 6
+print(x+y)
+print(x-y)
+#davaleba_7
+w = 15
+h = 16
+print(w+h)
+print(w*h)
+print(w-h)
+#davaleba_8
+k = 18
+z = 9
+print(k+z)
+print(k*z)
+print(k-z)
+#davaleba_9
+a = 23
+b = 24
+print(a+b)
+print(a*b)
+print(a-b)
+#davaleba_10
+a = 3
+b = 8
+c = 9
+print(a*b*c)
+print(a+b+c)
+print(a+b+c/3)
+#davaleba_11
+#a
+x = 5
+y = 8
+z = 12
+j = 9
+print((x+y)-(z*j))
+#b
+x = 5
+y = 8
+z =-12
+j = 11
+print(x/y+(z*j))
